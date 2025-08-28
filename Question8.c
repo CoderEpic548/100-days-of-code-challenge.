0af4dd716b7250880a1218990e23754971c4d9de
Pushkar <106814214+CoderEpic548@users.noreply.github.com>
2025-08-28 22:49:46 +0530

    Add files via upload

---
+++ Question8.c
@@ -0,0 +1,30 @@
+/*Q8: Write a program to find and display the sum of the first n natural numbers.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+Output 1:
+Sum=15
+
+Input 2:
+10
+Output 2:
+Sum=55
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n,sum=0;
+    printf("Enter the number till you want to add the series: ");
+    scanf("%d", &n);
+    for(int i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+    printf("The sum of series till n numbers is %d.",sum);
+    return 0;
+}
